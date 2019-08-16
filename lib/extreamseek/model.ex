d3a0c92409421a823472515c0e2bd40acd7f3a58
TrsNium <trsnium@gmail.com>
2019-08-16 23:54:30 +0900

    add test

--- lib/extreamseek/model.ex
+++ lib/extreamseek/model.ex
@@ -2,14 +2,19 @@
 
 defmodule ExtreamSeek.Dir do
   defstruct [:dir_name, :depth]
+
+  alias ExtreamSeek.Dir
+  def dirs_less_than_max(dirs, max) do
+    Enum.filter(dirs, fn (dir) -> dir.depth < max end)
+  end
 end
 
 
 defmodule ExtreamSeek.Path do
-  defstruct [:path, :is_contain]
+  defstruct path: nil, is_contain: false
 
-  def is_contain_paths(%ExtreamSeek.Path{} = paths) do
-    nil
+  def is_contain(%ExtreamSeek.Path{} = path) do
+    path.is_contain
   end
 end
 
@@ -23,6 +28,11 @@ defmodule ExtreamSeek.Process do
   end
   def is_all_completed(value), do: raise "Unexpected type is mixed: #{inspect(value)}"
 
+
+  defp get_processes_index_from_pid(pid, processes) do
+    Enum.find_index(processes, fn (process) -> Map.get(process, :pid) == pid end)
+  end
+
   # The execution status of the process is changed according to the received status.
   # If the received status is 'completed', 'completed_count' is incremented to indicate that the process is idling.
   # If 'execute', it means that a new job is being executed.
@@ -32,14 +42,10 @@ defmodule ExtreamSeek.Process do
 
     updated_process = case status do
       :completed ->
-        %Process{target_process | :completed_count => Map.get(target_process, :completed_count) + 1}
+        %Process{target_process | :completed_count => target_process.completed_count + 1}
       :execute ->
-        %Process{target_process | :total_count => Map.get(target_process, :total_count) + 1}
+        %Process{target_process | :total_count => target_process.total_count + 1}
     end
     List.delete_at(processes, index) ++ [updated_process]
-  end
-
-  defp get_processes_index_from_pid(pid, processes) do
-    Enum.find_index(processes, fn (process) -> Map.get(process, :pid) == pid end)
   end
 end
--- test/extream_seek_test.exs
+++ test/extream_seek_test.exs
@@ -6,7 +6,6 @@ defmodule ExtreamSeekTest do
     assert ExtreamSeek.hello() == :world
   end
 
-
   alias ExtreamSeek.Process
   @test_process_data [%Process{pid: 1, completed_count: 0, total_count: 0} , %Process{pid: 2, completed_count: 0, total_count: 0}]
   test "test ExtreamSeek.Process.is_all_completed" do
@@ -16,5 +15,11 @@ defmodule ExtreamSeekTest do
   test "test ExtreamSeek.Process.update_process" do
     updated = Process.update_process(1, @test_process_data, :execute)
     assert updated == [%Process{pid: 2, completed_count: 0, total_count: 0}, %Process{pid: 1, completed_count: 0, total_count: 1}]
+  end
+
+  @test_dir_data [%ExtreamSeek.Dir{dir_name: "test1", depth: 1}, %ExtreamSeek.Dir{dir_name: "test2", depth: 2}, %ExtreamSeek.Dir{dir_name: "test3", depth: 3}]
+  test "test ExtreamSeek.Dir.dirs_less_than_max" do
+    results = ExtreamSeek.Dir.dirs_less_than_max(@test_dir_data, 3)
+    assert results == [%ExtreamSeek.Dir{dir_name: "test1", depth: 1}, %ExtreamSeek.Dir{dir_name: "test2", depth: 2}]
   end
 end
